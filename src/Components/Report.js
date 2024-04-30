2f746a0658bc054e023ffdaa84f7ba3a7c3908ff
Jose Larrada <jalarrada@unicesar.edu.co>
2024-04-29 23:00:12 -0500

    Controlador de Reportes

--- src/Components/Report.js
+++ src/Components/Report.js
@@ -2,91 +2,14 @@ import React, { useEffect, useState } from 'react';
 import '../styles/repor.css'
 import {useNavigate} from 'react-router-dom'
 import verificarExpiracionToken from '../Configs/verificarExpiracionToken .js'
-
+import {generateGeneralReport, generateCustomReport, readReport} from '../controller/ReportController.js'
 function Report() {
   const tuToken = localStorage.getItem('token');
   const navigate= useNavigate();
   const [datos, setDatos] = useState([]);
-  const GuardarGeneral= async () => {
-    try {
-      if(!verificarExpiracionToken()){
-        navigate('/');
-      }
-      const response = await fetch('http://localhost:8080/api/v1/PrincipalContent/generate/document', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${tuToken}`
-        }
-      });
-
-      console.log(response);
-      if (response.ok) {
-        const data = await response.text();
-        alert(data);
-      }else{
-        const errorData = await response.text();
-        alert(errorData.token);
-      } 
-    } catch (error) {
-      alert('Error de red:', error);
-    }
-  };
-
-  //Informe Personalizado
-  const GuardarPersonalizado= async () => {
-    try {
-      if(!verificarExpiracionToken()){
-        navigate('/');
-      }
-      const url = `http://localhost:8080/api/v1/PrincipalContent/Generate/CustomDocument/${localStorage.getItem('id_ruta')}`;
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${tuToken}`
-        }
-      });
-      alert(localStorage.getItem('id_ruta'));
-      console.log(response);
-      if (response.ok) {
-        const data = await response.text();
-        alert(data);
-      }else{
-        const errorData = await response.text();
-        alert(errorData.token);
-      } 
-    } catch (error) {
-      alert('Error de red:', error);
-    }
-  };
-
-  const CargarPersonalizado= async () => {
-    try {
-      if(!verificarExpiracionToken()){
-        navigate('/');
-      }
-      const response = await fetch('http://localhost:8080/api/v1/PrincipalContent', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${tuToken}`
-        }
-      });
-      console.log(response);
-      if (response.ok) {
-        const data = await response.json();
-        setDatos(data);
-      }else{
-        const errorData = await response.text();
-        alert(errorData);
-      } 
-    } catch (error) {
-      alert('Error de red:', error);
-    }
-  };
+  
   useEffect(() => {
-    CargarPersonalizado();
+    readReport(verificarExpiracionToken,navigate,tuToken,setDatos);
   }, []);
 
   const handleClick = (item) => {
@@ -100,7 +23,7 @@ function Report() {
             <label>Personalizado</label>
         </div>
         <div className="nav-link active person ico control-move" to="/" >
-            <ion-icon onClick={GuardarGeneral} name="documents"></ion-icon>
+            <ion-icon onClick={()=>{generateGeneralReport(verificarExpiracionToken,navigate,tuToken)}} name="documents"></ion-icon>
             <label>General</label>
         </div>
         <div class="list-group">
@@ -108,8 +31,8 @@ function Report() {
             Proyectos
           </button>
           {datos.map((item, index) => (
-          <button key={index} type="button" class="list-group-item list-group-item-action" onClick={() => {handleClick(item);GuardarPersonalizado()}}>
-            {item.id_ruta}
+          <button key={index} type="button" class="list-group-item list-group-item-action" onClick={() => {handleClick(item);
+            generateCustomReport(verificarExpiracionToken,navigate,tuToken)}}>{item.id_ruta}
           </button>
         ))}
           <button type="button" class="list-group-item list-group-item-action" disabled>A disabled button item</button>
---
+++ src/controller/ReportController.js
@@ -0,0 +1,72 @@
+export const generateGeneralReport= async (verificarExpiracionToken,navigate,tuToken) => {
+    try {
+      if(!verificarExpiracionToken()){
+        navigate('/');
+      }
+      const response = await fetch('http://localhost:8080/api/v1/PrincipalContent/generate/document', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${tuToken}`
+        }
+      });
+      if (response.ok) {
+        const data = await response.text();
+        alert(data);
+      }else{
+        const errorData = await response.text();
+        alert(errorData);
+      } 
+    } catch (error) {
+      alert('Error de red:', error);
+    }
+};
+
+export const generateCustomReport= async (verificarExpiracionToken,navigate,tuToken) => {
+    try {
+      if(!verificarExpiracionToken()){
+        navigate('/');
+      }
+      const url = `http://localhost:8080/api/v1/PrincipalContent/Generate/CustomDocument/${localStorage.getItem('id_ruta')}`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${tuToken}`
+        }
+      });
+      if (response.ok) {
+        const data = await response.text();
+        alert(data);
+      }else{
+        const errorData = await response.text();
+        alert(errorData);
+      } 
+    } catch (error) {
+      alert('Error de red:', error);
+    }
+}; 
+
+export const readReport= async (verificarExpiracionToken,navigate,tuToken,setDatos) => {
+    try {
+      if(!verificarExpiracionToken()){
+        navigate('/');
+      }
+      const response = await fetch('http://localhost:8080/api/v1/PrincipalContent', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${tuToken}`
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setDatos(data);
+      }else{
+        const errorData = await response.text();
+        alert(errorData);
+      } 
+    } catch (error) {
+      alert('Error de red:', error);
+    }
+};
