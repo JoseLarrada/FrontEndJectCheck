b507a5317cbb619213bc5ed27b935c5ad1693101
Jose Larrada <jalarrada@unicesar.edu.co>
2024-05-03 10:30:02 -0500

    Componente de crear proyecto hecho

---
+++ src/Components/FormProject.js
@@ -0,0 +1,41 @@
+import {React, useState} from 'react'
+import '../styles/FormProject.css'
+
+function FormProject() {
+  const [Ischecked,setIschecked]=useState(false)
+  return (
+    <div className="formProjects">
+        <h3>Crear Proyectos</h3>
+        <section className="principalForm">
+            <input type="text" placeholder='Titulo del proyecto' required/>
+            <textarea name="Decription" id="" cols="30" rows="5" placeholder='Descripcion'></textarea>
+            <span className="findTeacher">
+                <input type="text" placeholder='Nombre del docente' required/>
+                <ion-icon name="search-outline"></ion-icon>
+            </span>
+            <span className="addAditional">
+              <input type="checkbox" id="miCheckbox" name="miCheckbox" value="valor" checked={Ischecked}
+              onChange={() => setIschecked(!Ischecked)}/>
+              <h6 for="miCheckbox">Adicionar Colaborador</h6>
+            </span>
+        </section>
+        <section className="aditionalOwners">
+          {Ischecked && (
+            <div>
+              <span className="findTeacher">
+                <input type="text" placeholder='Nombre del estudiante' required/>
+                <ion-icon name="search-outline"></ion-icon>
+              </span>
+              <span className="findTeacher">
+                <input type="text" placeholder='Nombre del estudiante' required/>
+                <ion-icon name="search-outline"></ion-icon>
+              </span>
+            </div>
+          )}
+        </section>
+        <button type="button" class="btn btn-outline-secondary">Guardar Proyecto</button>
+    </div>
+  )
+}
+
+export default FormProject
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import SideBarOption from '../Components/SideBarOption'
+import Nav from '../Components/Nav'
+import FormProject from '../Components/FormProject'
 
 function Projects() {
   return (
     <div>
+        <Nav/>
         <SideBarOption/>
+        <FormProject/>
     </div>
   )
 }
